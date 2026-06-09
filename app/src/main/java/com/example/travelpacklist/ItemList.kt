406aa473b142ebf1676989c8ad2ff3b9fc5c862e
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-06-09 21:38:05 +0200

    Receiving arrays from MainActivity intent

--- app/src/main/java/com/example/travelpacklist/ItemList.kt
+++ app/src/main/java/com/example/travelpacklist/ItemList.kt
@@ -1,6 +1,8 @@
 package com.example.travelpacklist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,18 @@ class ItemList : AppCompatActivity() {
         setContentView(R.layout.activity_item_list)
 
         //Linking the XML views to ItemList.kt
+        val txtList = findViewById<TextView>(R.id.txtList)
+        val btnDisplay = findViewById<Button>(R.id.btnDisplay)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        //Receiving the arrays passed from Main Activity
+        val items = intent.getStringArrayExtra("items") ?: emptyArray()
+        val quantities = intent.getIntArrayExtra("quantities") ?: intArrayOf()
+
+        //Displaying packing list button which runs when user clicks button
+
+            //Error Handling to check if items in array are missing
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
