0895e0549e38b41ac0418bcd68373946b710b7bc
Mattythelaw <matthewconnor0418@gmail.com>
2026-06-09 16:00:17 +0200

    Linked XML views with IDs to Main

--- app/src/main/java/com/example/travelpacklist/MainActivity.kt
+++ app/src/main/java/com/example/travelpacklist/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.travelpacklist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val itemDetails = findViewById<TextView>(R.id.itemDetails)
+        val packingBtn = findViewById<Button>(R.id.packingBtn)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
